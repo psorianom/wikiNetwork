7e4b41889bb7ebb769163b07dc70709903f9b050
Pavel <edmundo.soriano-morales@univ-lyon2.fr>
2015-02-08 17:50:47 +0100

    new branch: makematrix only with NPs

--- src/com/company/stanfordwiki2sparsematrix/MatrixMaker.java
+++ src/com/company/stanfordwiki2sparsematrix/MatrixMaker.java
@@ -49,175 +49,6 @@ public class MatrixMaker {
     }
 
 
-//    public MatrixContainer makeMatrix(String pathFile, MatrixContainer buildingMatrix) throws IOException {
-//
-//        String[] splittedLine;
-//        String token;
-//        String lemma;
-//        String posTag;
-//        String constituency;
-//        String dependencyHead;
-//        String dependency;
-//        int level = 1 + 1;
-////        HashMap<String,Integer> rowTokens = buildingMatrix.cMapTokenRow;
-//        HashMap<String, ArrayList<Integer>> posTokens = new HashMap<>();
-//        LinkedHashMap<String, ArrayList<String>> mapClausePOStags = new LinkedHashMap<>(); ///> Contains a hashmap of NP_12:[DT,JJ,NN] or whatever this NP_12 was made of
-//        ArrayList<Integer> rows = buildingMatrix.rows;///> Contains row indices
-//        ArrayList<Integer> cCols = buildingMatrix.cCols; ///> Contains columns indices
-//        ArrayList<Integer> data = new ArrayList<>(); ///> Contains the data
-//        HashSet<String> clausesSeen = new HashSet<>(); ///> Contains the type of clause found in the file. Only local, no need to return it.
-//        HashMap<String,HashMap<ArrayList<String>,ArrayList<Integer>>> cClauseSubClauseColumns = buildingMatrix.cClauseSubClauseColumns;
-//        ArrayList<String> lines = (ArrayList)Files.readAllLines(Paths.get(pathFile));
-//        HashMap<String, ArrayList<Integer>> mapSubClausesColumns = buildingMatrix.cSubClausesColumns;
-//        HashMap<String, ArrayList<Integer>> mapClausesColumns = buildingMatrix.cSubClausesColumns;
-//        lines.remove(0); ///> Remove the FILENAME header
-//        lines.remove(0); ///> Remove the columns headers
-//        for (String line:lines)
-//        {
-//            // it is a token. we deal with it.
-//            if (!line.contains("%%#"))
-//            {
-//                // We get the values from the csv line
-//                splittedLine = line.split("\t");
-//                token = splittedLine[0];
-//                lemma = splittedLine[1];
-//                posTag = splittedLine[2];
-//                constituency = splittedLine[3];
-//                dependencyHead = splittedLine[4];
-//                dependency = splittedLine[5];
-//                if (dependency.equals( "PUNCT"))
-//                    continue;
-//                /// Lets add the token to dictionary of row-index : token
-//                if (rowTokens.containsKey(lemma)) {
-//                    /// Lets add the token to the rows list
-//                    row_i = rowTokens.get(lemma);
-//                    rows.add(row_i);
-//
-//                }
-//                else {
-//                    rows.add(row_i);
-//                    //TODO: I should really get a defaultdict thingy over here...
-//                    rowTokens.put(lemma, row_i);
-//                }
-//
-//                //lets do the Constituency parsing
-//
-//                // second, we get the clause type (NP,VP, PRP, etc) we are interested in according to the level
-//                ArrayList<String> clauses = new ArrayList( Arrays.asList( constituency.split(",") ) );
-//
-//                if (clauses.size() < level)
-//                    level = clauses.size();
-//                String targetClause = clauses.get(clauses.size() - level);
-//                String[] clauseSplitted = targetClause.split("_");
-//                String clauseInitials = clauseSplitted[0];
-//                String clauseID = clauseSplitted[1];
-//                clausesSeen.add(clauseInitials);
-//
-//                // We get wh
-//                if (posTokens.containsKey(posTag))
-//                    posTokens.get(posTag).add(row_i);
-//                else
-//                    posTokens.put(posTag, new ArrayList(Arrays.asList(row_i)));
-//
-//
-//                if (mapClausePOStags.containsKey(targetClause))
-//                    mapClausePOStags.get(targetClause).add(posTag);
-//                else
-//                        mapClausePOStags.put(targetClause, new ArrayList(Arrays.asList(posTag)));
-//
-//                row_i = Collections.max(rows);
-//                row_i++;
-//            }
-//            else
-//            {
-//
-//                if (line.contains("SEN")) {
-//                    //we have the sentence id
-//                    String lineNb = line.split(" ")[1];
-//                    System.out.println("\t\tline: " +  lineNb);
-//                }
-//                else //line contains "PAGE"
-//                {
-//                    //we have the page name
-//                    System.out.println("PAGE: " +  line.split(" ")[1]);
-//                }
-//
-//
-//                if (row_i < 1)
-//                    continue;
-//                // We are done with the sentence, we actually assign values to the matrix
-//                //I get the relation between seen clause types (NP, VP,...) and the columns index
-//                Map<String, Integer> tuplesClauseColumn = clauseToIndices(new ArrayList<>(mapClausePOStags.keySet()));
-////                cCols = tuplesClauseColumn
-//                // We prepare a hashmap with the type of clause and the columns that correspond to each of them
-//                //  {NP_:[0,3,4], VP:[2,1]}
-//
-//                for (Map.Entry<String, Integer> entry : tuplesClauseColumn.entrySet()) {
-//                    String key = entry.getKey();
-//                    Integer value = entry.getValue();
-//                    if (mapSubClausesColumns.containsKey(key))
-//                        mapSubClausesColumns.get(key).add(value);
-//                    else
-//                        mapSubClausesColumns.put(key, new ArrayList(Arrays.asList(value)));
-//                }
-//
-//                for (String cl : clausesSeen)
-//                {
-//                    ArrayList<Integer> tempClauseColumn = new ArrayList<>();
-//                    for (Map.Entry<String, ArrayList<String>> entry : mapClausePOStags.entrySet())
-//                    {
-//                        String key = entry.getKey();
-//                        ArrayList<String> value = entry.getValue();
-//                        if (key.contains(cl+ "_"))
-//                        {
-//                            tempClauseColumn.addAll(mapSubClausesColumns.get(key));
-//                            if (cClauseSubClauseColumns.containsKey(cl))
-//                                if (cClauseSubClauseColumns.get(cl).containsKey(value))
-//                                    // We have the 'NP' first key, we have a second [DT,NN] key, now
-//                                    // we add the index of the columns that have this last two infos
-//                                    cClauseSubClauseColumns.get(cl).get(value).addAll(mapSubClausesColumns.get(key));
-//                                else
-//                                    cClauseSubClauseColumns.get(cl).put(value, new ArrayList(Arrays.asList(mapSubClausesColumns.get(key))));
-//                            else
-//                            {
-//                                HashMap<ArrayList<String>, ArrayList<Integer>> tempMap = new HashMap<>();
-//                                tempMap.put(value, new ArrayList(Arrays.asList(mapSubClausesColumns.get(key))));
-//                                // TODO: No se que estoy metiendo aqui. Esta mal.
-//                                cClauseSubClauseColumns.put(cl, tempMap);
-//                            }
-//
-//                            //This is another way to initialize a list inside a dict if there is no key. Like a defaultdict
-//                            // putIfabsent returns null if it does exist. otherwise it puts the key and value you indicate.
-//                            if (matrix.clauseColumns.containsKey(cl))
-//                                matrix.clauseColumns.get(cl).addAll(mapSubClausesColumns.get(key));
-//                            else
-//                                matrix.clauseColumns.put(cl,  new ArrayList(Arrays.asList(mapSubClausesColumns.get(key))));
-//
-//
-//
-////                            ArrayList<Integer> listTemp = matrix.clauseColumns.putIfAbsent(cl, tempClauseColumn);
-////                            if (listTemp != null)
-////                                listTemp.add(tuplesClauseColumn.get(key));
-//                        }
-//                    }
-//                }
-//                mapClausePOStags.clear();
-//                clausesSeen.clear();
-//            }
-//        }
-//
-//        matrix.rows = rows;
-//        matrix.cCols = cCols;
-//
-//        matrix.mapTokenRow = rowTokens;
-//
-//        matrix.cSubClausesColumns = mapSubClausesColumns;
-//
-//        matrix.cClauseSubClauseColumns = cClauseSubClauseColumns;
-//        return matrix;
-//
-//    }
-
     private void makeMatrix(String pathFile) throws IOException, InvalidLengthsException {
         String file = readFile(pathFile, true);
 
@@ -372,14 +203,22 @@ public class MatrixMaker {
 
         ///> This loops goes file by file
         for (String path : listPaths) {
+
             System.out.println(Integer.toString(idx) + ": " + path);
             //necesito aqui una funcion que me regrese  un objeto con todas las estructuras necesarias para armar la matriz
 //            buildingMatrix = makeMatrix(path,buildingMatrix);
             this.makeMatrix(path);
             idx++;
+            System.out.flush();
         }
 
-
+        if (this.matrix.cRows.size() != this.matrix.cCols.size())
+            throw new Utils.InvalidLengthsException("The length of vector i and j should be ALWAYS the same. Something is wrong...");
+        HashSet<Integer> hasho = new HashSet<>(this.matrix.cCols);
+        ///>Print some matrix statistics
+        String stats = String.format("Number of rows: %d\nNumber of columns: %d\nNumber of unique types of clauses: %d\nNumber of non-zero values: %d\n",
+                this.matrix.cMapTokenRow.keySet().size(), hasho.size(), this.matrix.cClauseSubClauseColumns.keySet().size(), this.matrix.cCols.size());
+        System.out.println(stats);
         long time = System.nanoTime() - start;
         System.out.println("Finished all threads");
         System.out.printf("Tasks took %.3f m to run%n", time / (60 * 1e9));
