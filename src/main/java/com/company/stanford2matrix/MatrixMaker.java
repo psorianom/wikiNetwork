79991b6d56a371ff6c9b3356813a14eda1747bfd
Pavel <edmundo.soriano-morales@univ-lyon2.fr>
2016-04-07 09:33:26 +0200

    MODIF: storing lemmas in cRowToken, cTokenRow and lListAllTokensPOS

--- src/main/java/com/company/stanford2matrix/MatrixMaker.java
+++ src/main/java/com/company/stanford2matrix/MatrixMaker.java
@@ -878,7 +878,7 @@ public class MatrixMaker {
                     String dependencyHead = splittedLine[4];
                     String dependency = splittedLine[5];
 //                    String token_pos = lemma + "_" + posTag;
-                    String token_pos = lemma + "_" + posTag;
+                    String token_pos = lemma;
                     lListAllTokensPOS.add(token_pos);
 
                     // HERE WE START. If the word is not a punctuation mark (PUNCT)
@@ -897,11 +897,11 @@ public class MatrixMaker {
                      *  1.a Add to the row list the current row i. rows[0,0,1,1...] for the i vector of the ijv sparse matrixContainer
                      */
 
-                    if (!matrixContainer.cTokenRow.containsKey(lemma)) {
+                    if (!matrixContainer.cTokenRow.containsKey(token_pos)) {
                         ///>This is the dict with the pos_tag : row_index
-                        matrixContainer.cTokenRow.put(lemma, row_i);
+                        matrixContainer.cTokenRow.put(token_pos, row_i);
                         ///> Save inverted index
-                        matrixContainer.cRowToken.put(row_i, lemma);
+                        matrixContainer.cRowToken.put(row_i, token_pos);
                         matrixContainer.cPOSToken.get(posTag).add(row_i);
                         row_i++;
 
@@ -1167,7 +1167,7 @@ public class MatrixMaker {
                         ///>This is the dict with the pos_tag : row_index
                         matrixContainer.cTokenRow.put(token_pos, row_i);
                         /// Save inverted index
-                        matrixContainer.cRowToken.put(row_i, lemma);
+                        matrixContainer.cRowToken.put(row_i, lemma); ///> WTF PAVEL! why dint i put lemma also in cTokenRow???
                         matrixContainer.cPOSToken.get(posTag).add(row_i);
                         row_i++;
 
@@ -1367,6 +1367,8 @@ public class MatrixMaker {
 
         /// There is only one file here, the semeval 2010 XML
         for (String path : listPaths) {
+            if (!path.contains("moment"))
+                continue;
             System.out.println(Integer.toString(idx) + ": " + path);
             this.prepareSemeval2010TrainMatrixNPs(path);
             idx++;
