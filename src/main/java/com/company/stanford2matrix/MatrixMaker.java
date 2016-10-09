30d4bf041d80339a234d8a509b4ebef3c403cf60
Pavel <edmundo.soriano-morales@univ-lyon2.fr>
2016-10-09 15:24:07 +0200

    REFACTOR:Went to double linking ways

--- src/main/java/com/company/stanford2matrix/MatrixMaker.java
+++ src/main/java/com/company/stanford2matrix/MatrixMaker.java
@@ -907,8 +907,8 @@ public class MatrixMaker {
 
                     }
                     //Here we add the dependencies
-//                    addSingleLinkDependenciesColumns(lListAllTokensPOS, lDictDependencyHeadIndex);
-                    addDoubleLinkDependenciesColumns(lListAllTokensPOS, lDictDependencyHeadIndex);
+                    addSingleLinkDependenciesColumns(lListAllTokensPOS, lDictDependencyHeadIndex);
+//                    addDoubleLinkDependenciesColumns(lListAllTokensPOS, lDictDependencyHeadIndex);
 
 
                     //Here we add the ngrams columns
